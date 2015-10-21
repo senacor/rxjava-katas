850d1c375b486442a2fde26bce7bf932cf96b89e
Mihael Gorupec <Mihael.Gorupec@Senacor.com>
2015-10-21 13:17:11 +0200

    completed kata 1

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
@@ -3,6 +3,7 @@ package com.senacor.tecco.codecamp.reactive.katas;
 import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import org.junit.Test;
+import rx.Observable;
 
 /**
  * @author Andreas Keefer
@@ -13,7 +14,36 @@ public class Kata1CreateObservable {
     public void erzeugeEinObservable() throws Exception {
         final String articleName = "Observable";
         // Erzeuge aus getArticle ein Observable
+        Observable.<Article>create(subscriber -> {
 
+            try {
+                subscriber.onNext(getArticle(articleName));
+                subscriber.onNext(getArticle("Physik"));
+                subscriber.onNext(getArticle("Beratung"));
+                subscriber.onCompleted();
+            } catch (Exception e) {
+                subscriber.onError(new IllegalArgumentException());
+            }
+        })
+                .map(article -> article.getText().length())
+                .subscribe(
+                        item -> System.out.println(item), //on next
+                        Throwable::printStackTrace, //on error
+                        () -> System.out.println("completed")//on complete
+                );
+    }
+
+    @Test
+    public void useJust() {
+        final String articleName = "Observable";
+        // Erzeuge aus getArticle ein Observable
+        Observable.defer(() -> Observable.just(getArticle(articleName))
+                .map(article -> article.getText().length())
+        ).subscribe(
+                item -> System.out.println(item), //on next
+                Throwable::printStackTrace, //on error
+                () -> System.out.println("completed")//on complete
+        );
     }
 
     public Article getArticle(String name) {
