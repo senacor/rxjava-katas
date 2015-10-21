61cfca5471baaa57ecfa6c920dfd4b2b19b2f917
chtw <chtw>
2015-10-21 16:58:06 +0200

    Kata6

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata6WikiLinks.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata6WikiLinks.java
@@ -1,6 +1,21 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
+import static rx.Observable.defer;
+import static rx.Observable.from;
+
+import java.util.concurrent.TimeUnit;
+
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
+import com.senacor.tecco.codecamp.reactive.services.WikiService;
+
 import org.junit.Test;
+
+import de.tudarmstadt.ukp.wikipedia.parser.Link;
+import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
 
 /**
  * @author Andreas Keefer
@@ -15,7 +30,27 @@ public class Kata6WikiLinks {
         // 4. miss die Performance und optimiere die Performance mit Schedulern
         // 5. gib keine Kombination <Start_Artikel> -> <Link/Artikel_Name> mehrfach aus
 
-        //WikiService.WIKI_SERVICE.fetchArticle(...);
+        WaitMonitor waitMonitor = new WaitMonitor();
+
+        String startArticle = "Observable";
+        getLinksFromArticle(startArticle)
+                .map(link -> link.getTarget())
+                .distinct()
+                .flatMap(articleName -> getLinksFromArticle(articleName))
+                .map(link -> link.getTarget())
+                .distinct()
+                .subscribe(link -> System.out.println(startArticle + " -> " + link),
+                        error -> error.printStackTrace(),
+                        () -> waitMonitor.complete());
+
+        waitMonitor.waitFor(50000L, TimeUnit.SECONDS);
+    }
+
+    private Observable<Link> getLinksFromArticle(String articleName) {
+        return defer(() -> WIKI_SERVICE.fetchArticle(articleName).subscribeOn(Schedulers.io()))
+                .flatMap(article -> WIKI_SERVICE.parseMediaWikiText(article).subscribeOn(Schedulers.computation()))
+                .flatMap(page -> from(page.getSections()))
+                .flatMap(section -> from(section.getLinks(Link.type.INTERNAL)));
     }
 
 }
