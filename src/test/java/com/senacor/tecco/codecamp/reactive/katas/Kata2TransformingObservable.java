88efc230ac70b6b8e1b9ba3921cfbdc12c3ce5a2
Sebastian Rühl <sebastian.ruehl@senacor.com>
2015-10-21 13:31:43 +0200

    kata 2 with ernie

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata2TransformingObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata2TransformingObservable.java
@@ -1,19 +1,33 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
+import com.senacor.tecco.codecamp.reactive.services.WikiService;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Andreas Keefer
  */
 public class Kata2TransformingObservable {
 
-    @Test
-    public void transformingObservable() throws Exception {
-        // 1. Benutze den WikiService (fetchArticle) und hole dir einen beliebigen Wikipedia Artikel
-        // 2. Transformiere das Ergebnis mit Hilfe von WikiService#parseMediaWikiText in eine Objektstruktur
-        // 3. gib den Wikipedia Artikel Text in der Console aus (ParsedPage.getText())
+  @Test
+  public void transformingObservable() throws Exception {
+    // 1. Benutze den WikiService (fetchArticle) und hole dir einen beliebigen Wikipedia Artikel
+    // 2. Transformiere das Ergebnis mit Hilfe von WikiService#parseMediaWikiText in eine Objektstruktur
+    // 3. gib den Wikipedia Artikel Text in der Console aus (ParsedPage.getText())
 
-        // WikiService.WIKI_SERVICE.fetchArticle()
-    }
+    WaitMonitor waitMonitor = new WaitMonitor();
+
+    WikiService.WIKI_SERVICE.fetchArticle("Banane")
+                            .flatMap(WikiService.WIKI_SERVICE::parseMediaWikiText)
+                            .map(ParsedPage::getText)
+                            .subscribe(System.out::println,
+                                       Throwable::printStackTrace,
+                                       waitMonitor::complete);
+
+    waitMonitor.waitFor(5, TimeUnit.SECONDS);
+  }
 
 }
