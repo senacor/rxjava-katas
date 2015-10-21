66d5a10830b149e0552a660a0110ed50a0d63090
Sebastian Rühl <sebastian.ruehl@senacor.com>
2015-10-21 14:52:18 +0200

    kata 4 with ernie

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata4FilteringObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata4FilteringObservable.java
@@ -1,5 +1,6 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,20 +12,38 @@ import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERV
  */
 public class Kata4FilteringObservable {
 
-    @Test
-    public void filterObservable() throws Exception {
-        // 1. Benutze den WikiService#wikiArticleBeingReadObservable, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden
-        // 2. Filtere die Name so, dass nur Artikel mit mindestens 15 Buchstaben akzeptiert werden und gib alles auf der Console aus
+  @Test
+  public void filterObservable() throws Exception {
+    // 1. Benutze den WikiService#wikiArticleBeingReadObservable, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden
+    // 2. Filtere die Name so, dass nur Artikel mit mindestens 15 Buchstaben akzeptiert werden und gib alles auf der Console aus
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(500, TimeUnit.MILLISECONDS);
-    }
+    WaitMonitor waitMonitor = new WaitMonitor();
 
-    @Test
-    public void filterObservable2() throws Exception {
-        // 1. Benutze den WikiService#wikiArticleBeingReadObservable, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden
-        // 2. Der Stream liefert uns zu viel Artikel, so schnell koennen wir die Artikel nicht bearbeiten.
-        //    Beschränke den Stream auf einen Artikel alle 500ms. Direkt die Parameter am wikiArticleBeingReadObservable zu Ändern gilt natürlich nicht ;)
+    WIKI_SERVICE.wikiArticleBeingReadObservable(500, TimeUnit.MILLISECONDS)
+                .flatMap(WIKI_SERVICE::parseMediaWikiText)
+                .filter(parsedPage -> parsedPage.getText().length() > 15)
+                .subscribe(System.out::println,
+                           Throwable::printStackTrace,
+                           waitMonitor::complete);
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(100, TimeUnit.MILLISECONDS);
-    }
+    waitMonitor.waitFor(5, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void filterObservable2() throws Exception {
+    // 1. Benutze den WikiService#wikiArticleBeingReadObservable, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden
+    // 2. Der Stream liefert uns zu viel Artikel, so schnell koennen wir die Artikel nicht bearbeiten.
+    //    Beschränke den Stream auf einen Artikel alle 500ms. Direkt die Parameter am wikiArticleBeingReadObservable zu Ändern gilt natürlich nicht ;)
+
+    WaitMonitor waitMonitor = new WaitMonitor();
+
+    WIKI_SERVICE.wikiArticleBeingReadObservable(100, TimeUnit.MILLISECONDS)
+                .flatMap(WIKI_SERVICE::parseMediaWikiText)
+                .sample(500, TimeUnit.MILLISECONDS)
+                .subscribe(System.out::println,
+                           Throwable::printStackTrace,
+                           waitMonitor::complete);
+
+    waitMonitor.waitFor(5, TimeUnit.SECONDS);
+  }
 }
