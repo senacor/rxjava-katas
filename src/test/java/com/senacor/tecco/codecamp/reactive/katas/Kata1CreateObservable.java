0af8d9311f4d9d62943afc1bb21e2c5121674334
Christopher Gertz <Christopher.Gertz@senacor.com>
2015-10-21 13:32:49 +0200

    Kata2

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
@@ -1,22 +1,72 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
-import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
-import net.sourceforge.jwbf.core.contentRep.Article;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
+import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
+
+import net.sourceforge.jwbf.core.contentRep.Article;
+import rx.Observable;
 
 /**
  * @author Andreas Keefer
  */
 public class Kata1CreateObservable {
 
-    @Test
-    public void erzeugeEinObservable() throws Exception {
-        final String articleName = "Observable";
-        // Erzeuge aus getArticle ein Observable
+	@Test
+	public void erzeugeEinObservable() throws Exception {
+		final String articleName = "Observable";
+		// Erzeuge aus getArticle ein Observable
 
-    }
+		WaitMonitor monitor = new WaitMonitor();
 
-    public Article getArticle(String name) {
-        return new WikipediaServiceMediaWikiBot().getArticle(name);
-    }
+		Observable.<Article>create(subscriber -> {
+			try {
+				subscriber.onNext(getArticle(articleName));
+				subscriber.onNext(getArticle("Physik"));
+				subscriber.onNext(getArticle("Beratung"));
+			} catch (Exception e) {
+				subscriber.onError(new NullPointerException("Some error occured"));
+			}
+			subscriber.onCompleted();
+		})
+		.map(article -> article.getText().length())
+		.subscribe(
+			length -> System.out.println(length), // on next
+//			article -> System.out.println(article.getText()), // on next
+			error -> error.printStackTrace(),
+			//Throwable::printStackTrace, // on error
+			() -> {System.out.println("completed"); monitor.complete();} // on completed
+		);
+		
+		monitor.waitFor(15L, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void useJust() {
+		final String articleName = "Observable";
+		// Erzeuge aus getArticle ein Observable
+		
+		WaitMonitor monitor = new WaitMonitor();
+
+//		Observable.just(getArticle(articleName))
+		Observable.defer(() -> Observable.just(getArticle(articleName)))
+		.map(article -> article.getText().length())
+		.subscribe(
+			length -> System.out.println(length), // on next
+//			article -> System.out.println(article.getText()), // on next
+			error -> error.printStackTrace(),
+			//Throwable::printStackTrace, // on error
+			() -> {System.out.println("completed"); monitor.complete();} // on completed
+		);
+		
+		monitor.waitFor(15L, TimeUnit.SECONDS);
+		
+	}
+
+	public Article getArticle(String name) {
+		return new WikipediaServiceMediaWikiBot().getArticle(name);
+	}
 }
--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata2TransformingObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata2TransformingObservable.java
@@ -2,18 +2,27 @@ package com.senacor.tecco.codecamp.reactive.katas;
 
 import org.junit.Test;
 
+import com.senacor.tecco.codecamp.reactive.services.WikiService;
+
+import rx.Observable;
+
 /**
  * @author Andreas Keefer
  */
 public class Kata2TransformingObservable {
 
-    @Test
-    public void transformingObservable() throws Exception {
-        // 1. Benutze den WikiService (fetchArticle) und hole dir einen beliebigen Wikipedia Artikel
-        // 2. Transformiere das Ergebnis mit Hilfe von WikiService#parseMediaWikiText in eine Objektstruktur
-        // 3. gib den Wikipedia Artikel Text in der Console aus (ParsedPage.getText())
-
-        // WikiService.WIKI_SERVICE.fetchArticle()
-    }
+	@Test
+	public void transformingObservable() throws Exception {
+		// 1. Benutze den WikiService (fetchArticle) und hole dir einen
+		// beliebigen Wikipedia Artikel
+		WikiService.WIKI_SERVICE.fetchArticle("Elektrotechnik")
+		// 2. Transformiere das Ergebnis mit Hilfe von WikiService#parseMediaWikiText in eine Objektstruktur
+				.flatMap(str -> WikiService.WIKI_SERVICE.parseMediaWikiText(str))
+				// 3. gib den Wikipedia Artikel Text in der Console aus
+				.subscribe(text -> System.out.println(text.getText()));
+		//
+		
+		Thread.sleep(10000L);
+	}
 
 }
