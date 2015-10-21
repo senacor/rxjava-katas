e9af0bef768219a2417f5f4a6834f40cdbf031a7
Sebastian Rühl <sebastian.ruehl@senacor.com>
2015-10-21 13:14:32 +0200

    kata 1

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
@@ -1,22 +1,40 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
 import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import org.junit.Test;
+import rx.Observable;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Andreas Keefer
  */
 public class Kata1CreateObservable {
 
-    @Test
-    public void erzeugeEinObservable() throws Exception {
-        final String articleName = "Observable";
-        // Erzeuge aus getArticle ein Observable
+  @Test
+  public void erzeugeEinObservable() throws Exception {
+    final String articleName = "Observable";
+    // Erzeuge aus getArticle ein Observable
 
-    }
+    WaitMonitor waitMonitor = new WaitMonitor();
 
-    public Article getArticle(String name) {
-        return new WikipediaServiceMediaWikiBot().getArticle(name);
-    }
+    Observable.<Article>create(subscriber -> {
+      subscriber.onNext(getArticle(articleName));
+      subscriber.onNext(getArticle("Banane"));
+      subscriber.onNext(getArticle("Annanas"));
+      subscriber.onCompleted();
+    })
+              .map(Article::getText)
+              .subscribe(System.out::println,
+                         Throwable::printStackTrace,
+                         waitMonitor::complete);
+
+    waitMonitor.waitFor(5, TimeUnit.SECONDS);
+  }
+
+  public Article getArticle(String name) {
+    return new WikipediaServiceMediaWikiBot().getArticle(name);
+  }
 }
