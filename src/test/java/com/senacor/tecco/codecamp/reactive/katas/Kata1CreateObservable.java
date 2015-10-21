e9373b139d5ac7063be7aab5cba6af89a2e7e736
sourcekick <sven.ludwig@senacor.com>
2015-10-21 13:28:33 +0200

    kata 1 done

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
@@ -3,6 +3,7 @@ package com.senacor.tecco.codecamp.reactive.katas;
 import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import org.junit.Test;
+import rx.Observable;
 
 /**
  * @author Andreas Keefer
@@ -13,6 +14,40 @@ public class Kata1CreateObservable {
     public void erzeugeEinObservable() throws Exception {
         final String articleName = "Observable";
         // Erzeuge aus getArticle ein Observable
+
+        Observable.create(subscriber -> {
+            subscriber.onNext(getArticle(articleName));
+            subscriber.onCompleted();
+        }).subscribe(
+                // handle item
+                article -> System.out.println(article),
+                Throwable::printStackTrace,
+                () -> System.out.println("erzeugeEinObservable Completed.")
+                // on next
+                // on error
+                // on complete
+        );
+
+        Observable.<Article>defer(() -> Observable.create(subscriber -> {
+            try {
+
+                subscriber.onNext(getArticle(articleName));
+                subscriber.onNext(getArticle("Physik"));
+                subscriber.onNext(getArticle("Mathematik"));
+                subscriber.onCompleted();
+            } catch (Exception e) {
+                subscriber.onError(new RuntimeException("Something went wrong. Disconnect from Observable."));
+            }
+        })).map(article -> article.getText().length())
+                .subscribe(
+                        // handle item
+                        article -> System.out.println(article),
+                        Throwable::printStackTrace,
+                        () -> System.out.println("erzeugeEinObservable Completed.")
+                        // on next
+                        // on error
+                        // on complete
+                );
 
     }
 
