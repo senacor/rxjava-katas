2377ecb5c189956389a7dbf4f32b316b64d2f0d4
Wolfgang Fischer <wolfgang.fischer@senacor.com>
2015-10-21 10:44:37 +0200

    test removed

--- src/main/java/com/senacor/tecco/codecamp/reactive/WaitMonitor.java
+++ src/main/java/com/senacor/tecco/codecamp/reactive/WaitMonitor.java
@@ -11,7 +11,7 @@ import static com.senacor.tecco.codecamp.reactive.ReactiveUtil.print;
  * @author Andreas Keefer
  */
 public class WaitMonitor {
-    // Test2
+
     private final LinkedBlockingQueue<Boolean> queue = new LinkedBlockingQueue<>();
     private final long creationTimeStamp = System.currentTimeMillis();
     private Long durationToCompleteInMs = null;
