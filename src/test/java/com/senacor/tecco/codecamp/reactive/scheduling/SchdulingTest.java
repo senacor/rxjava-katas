659e529c72e004458b71062543209855e7c2f5be
Keefer, Andreas <Andreas.Keefer@senacor.com>
2015-11-13 14:26:42 +0100

    Schduling visualisazion

--- src/test/java/com/senacor/tecco/codecamp/reactive/scheduling/SchdulingTest.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/scheduling/SchdulingTest.java
@@ -1,20 +1,63 @@
 package com.senacor.tecco.codecamp.reactive.scheduling;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.senacor.tecco.codecamp.reactive.WaitMonitor;
 import org.junit.Test;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.schedulers.Schedulers;
 
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.senacor.tecco.codecamp.reactive.ReactiveUtil.print;
 
 /**
  * @author Andreas Keefer
+ * @see http://reactivex.io/documentation/scheduler.html
  */
 public class SchdulingTest {
+
+    @Test
+    public void testVisualizeSubscribeOnAndObserveOn() throws Exception {
+        Thread.currentThread().setName("Main");
+
+        Scheduler thread1 = Schedulers.from(Executors.newFixedThreadPool(1,
+                new ThreadFactoryBuilder().setNameFormat("Thread1").build()));
+        Scheduler thread2 = Schedulers.from(Executors.newFixedThreadPool(1,
+                new ThreadFactoryBuilder().setNameFormat("Thread2").build()));
+        Scheduler thread3 = Schedulers.from(Executors.newFixedThreadPool(1,
+                new ThreadFactoryBuilder().setNameFormat("Thread3").build()));
+
+        final WaitMonitor monitor = new WaitMonitor();
+        Subscription subscription = Observable.<Integer>create(subscriber -> {
+            print("create");
+            subscriber.onNext(1);
+            subscriber.onCompleted();
+        }).subscribeOn(thread1)
+                .map(next -> {
+                    print("map: %s", next);
+                    return next;
+                })
+                .observeOn(thread2)
+                .flatMap(next -> {
+                    print("flatMap: %s", next);
+                    return Observable.just(next);
+                })
+                .observeOn(thread3)
+                .subscribe(next -> print("Observer: %s", next),
+                        Throwable::printStackTrace,
+                        () -> {
+                            print("complete!");
+                            monitor.complete();
+                        });
+
+        monitor.waitFor(1000, TimeUnit.MILLISECONDS);
+        subscription.unsubscribe();
+    }
+
     @Test
     public void testSubscribeOn() throws Exception {
         final WaitMonitor monitor = new WaitMonitor();
