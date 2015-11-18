c95176c0d8dd0b5cb99f0e2c9f5a9317171c655d
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-18 15:19:11 +0100

    solved kata4

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata4FilteringObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata4FilteringObservable.java
@@ -1,5 +1,6 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -16,7 +17,16 @@ public class Kata4FilteringObservable {
         // 1. Benutze den WikiService#wikiArticleBeingReadObservable, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden
         // 2. Filtere die Name so, dass nur Artikel mit mindestens 15 Buchstaben akzeptiert werden und gib alles auf der Console aus
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(500, TimeUnit.MILLISECONDS);
+        WaitMonitor waitMonitor = new WaitMonitor();
+
+        WIKI_SERVICE
+                .wikiArticleBeingReadObservable(500, TimeUnit.MILLISECONDS)
+                //.filter(articleName -> articleName.length() >= 15)
+                .flatMap(WIKI_SERVICE::fetchArticle)
+                //.flatMap(WIKI_SERVICE::parseMediaWikiText)
+                .subscribe(System.out::println, Throwable::printStackTrace, waitMonitor::complete);
+
+        waitMonitor.waitFor(20, TimeUnit.SECONDS);
     }
 
     @Test
@@ -25,6 +35,16 @@ public class Kata4FilteringObservable {
         // 2. Der Stream liefert uns zu viel Artikel, so schnell koennen wir die Artikel nicht bearbeiten.
         //    Beschränke den Stream auf einen Artikel alle 500ms. Direkt die Parameter am wikiArticleBeingReadObservable zu Ändern gilt natürlich nicht ;)
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(100, TimeUnit.MILLISECONDS);
+        WaitMonitor waitMonitor = new WaitMonitor();
+
+        WIKI_SERVICE
+                .wikiArticleBeingReadObservable(100, TimeUnit.MILLISECONDS)
+                //.filter(articleName -> articleName.length() >= 15)
+                .flatMap(WIKI_SERVICE::fetchArticle)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                //.flatMap(WIKI_SERVICE::parseMediaWikiText)
+                .subscribe(System.out::println, Throwable::printStackTrace, waitMonitor::complete);
+
+        waitMonitor.waitFor(20, TimeUnit.SECONDS);
     }
 }
