60d6a354491dac158e54c9bf2721cff107b2f778
Alasdair Collinson <blalasaadri@googlemail.com>
2015-11-18 14:34:33 +0100

    Dritte Aufgabe gelöst

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata3CombiningObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata3CombiningObservable.java
@@ -1,6 +1,14 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import static com.senacor.tecco.codecamp.reactive.ReactiveUtil.print;
+import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
+
 import org.junit.Test;
+
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
+import rx.Observable;
 
 /**
  * @author Andreas Keefer
@@ -14,6 +22,31 @@ public class Kata3CombiningObservable {
         //    und gib das JSON auf der Console aus. Beispiel {"articleName": "Superman", "rating": 3, "wordCount": 452}
 
         // WikiService.WIKI_SERVICE.fetchArticle()
+        final String articleName = "Abrollrichtung_von_Toilettenpapier";
+
+        final Observable<ParsedPage> articleObservable = WIKI_SERVICE.fetchArticle(articleName)
+            .doOnNext(debug -> print("fetchArticle res: %s", debug))
+            .flatMap(WIKI_SERVICE::parseMediaWikiText)
+            .doOnNext(debug -> print("parseArticle res: %s", debug));
+
+        articleObservable.flatMap(WIKI_SERVICE::rate)
+            .doOnNext(debug -> print("rate: %d", debug))
+            .zipWith(articleObservable.flatMap(WIKI_SERVICE::countWords).doOnNext(debug -> print("count: %d", debug)), ArticleInfos::new)
+            .doOnNext(debug -> print("zipping: %s", debug))
+            .subscribe(articleInfos -> {
+                ReactiveUtil
+                    .print("{\"articleName\": \"%s\", \"rating\": %d, \"wordCount\": %d}", articleName, articleInfos.rate, articleInfos.count);
+            }, ReactiveUtil::print);
+    }
+
+    private class ArticleInfos {
+        private final Integer rate;
+        private final Integer count;
+
+        private ArticleInfos(Integer rate, Integer count) {
+            this.rate = rate;
+            this.count = count;
+        }
     }
 
 }
