5d95d757a327a96dc3d00559ef4985087cb0ea5d
Philipp Klose <pklose@KLOPH-AMBP.local>
2015-11-18 16:18:18 +0100

    Completed task 5 - updated

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata5SchedulingObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata5SchedulingObservable.java
@@ -1,6 +1,11 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
 import org.junit.Test;
+import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +26,25 @@ public class Kata5SchedulingObservable {
         // 5. messe die Laufzeit
         // 6. Füge jetzt an passender Stelle in der Observable-Chain einen Schduler ein um die Ausführungszeit zu verkürzen
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(50, TimeUnit.MILLISECONDS);
+
+        Scheduler myScheduler = ReactiveUtil.newScheduler(5, "myScheduler");
+
+        final WaitMonitor monitor = new WaitMonitor();
+        WIKI_SERVICE.wikiArticleBeingReadObservable(50, TimeUnit.MILLISECONDS).take(20)
+        .flatMap(WIKI_SERVICE::fetchArticle).subscribeOn(myScheduler).
+                flatMap(WIKI_SERVICE::parseMediaWikiText)
+                .flatMap(page -> {
+                    Observable<Integer> countedWords = WIKI_SERVICE.countWords(page).subscribeOn(myScheduler);
+                    Observable<Integer> pageRate = WIKI_SERVICE.rate(page).subscribeOn(myScheduler);
+
+                    return Observable.zip(countedWords, pageRate, (wo, ra) -> {
+                        return String.format("ArticleName:Java, rating:%s, wordCount: %s", wo, ra);
+                    });
+                }).subscribeOn(myScheduler).subscribe(json -> {
+            ReactiveUtil.print(json);
+        }, Throwable::printStackTrace, () -> monitor.complete());
+
+        monitor.waitFor(10, TimeUnit.SECONDS);
     }
 
 }
