a1eee16863c43aa94b578f868d00cdfa02dc8732
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-18 17:56:00 +0100

    solved kata8

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata8Batch.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata8Batch.java
@@ -1,6 +1,9 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
 import org.junit.Test;
+import rx.Observable;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
 
@@ -11,12 +14,18 @@ public class Kata8Batch {
 
     @Test
     public void withoutBatch() throws Exception {
-        // 1. Benutze den WikiService#wikiArticleBeingReadObservableBurst, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden.
+        // 1. Benutze den WikiService#wikiArticleBeingReadObservableBurst, der einen Stream von WikiArtikel Namen liefert,
+        // die gerade gelesen werden.
         // 2. beobachte den Stream 2 sekunden lang
         // 3. speichere die Artikel ab (WikiService.save(String)). Der service gibt die laufzeit zurück
         // 4. summiere die laufzeit der save calls und gib dies aus
 
-        WIKI_SERVICE.wikiArticleBeingReadObservableBurst();
+        WIKI_SERVICE.wikiArticleBeingReadObservableBurst()
+                .take(2, TimeUnit.SECONDS)
+                .map(WIKI_SERVICE::save)
+                .reduce((l, r) -> l + r)
+                .subscribe(System.out::println, Throwable::printStackTrace);
+        Thread.sleep(10000);
     }
 
 
@@ -26,6 +35,12 @@ public class Kata8Batch {
         //    Artikeln zu speichern. Beachte dabei, dass du hier potentiel einen Stream hast, du kannst also nicht warten
         //    bis der Stream alle artikel geliefert hat und dann alles in einem grossen batch abspeichern
 
-        WIKI_SERVICE.wikiArticleBeingReadObservableBurst();
+        WIKI_SERVICE.wikiArticleBeingReadObservableBurst()
+                .take(2, TimeUnit.SECONDS)
+                .buffer(10)
+                .map(WIKI_SERVICE::save)
+                .reduce((l, r) -> l + r)
+                .subscribe(System.out::println, Throwable::printStackTrace);
+        Thread.sleep(10000);
     }
 }
