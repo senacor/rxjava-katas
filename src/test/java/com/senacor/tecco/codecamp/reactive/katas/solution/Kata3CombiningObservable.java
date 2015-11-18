efdd9acd505562359cf1273642b60c8a84d10e34
Keefer, Andreas <Andreas.Keefer@senacor.com>
2015-11-18 09:32:17 +0100

    cleanup

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/solution/Kata3CombiningObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/solution/Kata3CombiningObservable.java
@@ -26,10 +26,6 @@ public class Kata3CombiningObservable {
 
         WaitMonitor waitMonitor = new WaitMonitor();
 
-        Scheduler rateScheduler = newScheduler(3, "rate-scheduler");
-        Scheduler countScheduler = newScheduler(3, "count-scheduler");
-        Scheduler myScheduler = newScheduler(3, "my-scheduler");
-
         final String wikiArticle = "Bilbilis";
         WIKI_SERVICE.fetchArticle(wikiArticle)
                 .flatMap(WIKI_SERVICE::parseMediaWikiText)
@@ -40,7 +36,6 @@ public class Kata3CombiningObservable {
                             "{\"articleName\": \"%s\", \"rating\": %s, \"wordCount\": %s}",
                             wikiArticle, r, wc));
                 })
-                .subscribeOn(myScheduler)
                 .subscribe(next -> print("next: %s", next),
                         Throwable::printStackTrace,
                         () -> waitMonitor.complete());
