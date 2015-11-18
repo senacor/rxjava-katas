695617699a3ac5c01d6de8ab4b21648ed5d8b5ef
Alasdair Collinson <blalasaadri@googlemail.com>
2015-11-18 15:01:47 +0100

    Vierte Aufgabe gelöst

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata4FilteringObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata4FilteringObservable.java
@@ -1,10 +1,13 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
-import org.junit.Test;
+import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
+import org.junit.Test;
+
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
 
 /**
  * @author Andreas Keefer
@@ -16,7 +19,12 @@ public class Kata4FilteringObservable {
         // 1. Benutze den WikiService#wikiArticleBeingReadObservable, der einen Stream von WikiArtikel Namen liefert, die gerade gelesen werden
         // 2. Filtere die Name so, dass nur Artikel mit mindestens 15 Buchstaben akzeptiert werden und gib alles auf der Console aus
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(500, TimeUnit.MILLISECONDS);
+        WaitMonitor waitMonitor = new WaitMonitor();
+
+        WIKI_SERVICE.wikiArticleBeingReadObservable(500, TimeUnit.MILLISECONDS).filter(name -> name.length() >= 15)
+            .subscribe(name -> ReactiveUtil.print("wikiArticleWithAtLeast15Characters=%s", name), Throwable::printStackTrace, waitMonitor::complete);
+
+        waitMonitor.waitFor(10, TimeUnit.SECONDS);
     }
 
     @Test
@@ -25,6 +33,13 @@ public class Kata4FilteringObservable {
         // 2. Der Stream liefert uns zu viel Artikel, so schnell koennen wir die Artikel nicht bearbeiten.
         //    Beschränke den Stream auf einen Artikel alle 500ms. Direkt die Parameter am wikiArticleBeingReadObservable zu Ändern gilt natürlich nicht ;)
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(100, TimeUnit.MILLISECONDS);
+        WaitMonitor waitMonitor = new WaitMonitor();
+
+        WIKI_SERVICE.wikiArticleBeingReadObservable(100, TimeUnit.MILLISECONDS)
+            .filter(name -> name.length() >= 15)
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .subscribe(name -> ReactiveUtil.print("wikiArticleWithAtLeast15Characters=%s", name), Throwable::printStackTrace, waitMonitor::complete);;
+
+        waitMonitor.waitFor(10, TimeUnit.SECONDS);
     }
 }
