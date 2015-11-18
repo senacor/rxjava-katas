eca896d934835ee57c1a9bf1dbd6d6e12a05787a
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-18 16:29:07 +0100

    solved kata5

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata5SchedulingObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata5SchedulingObservable.java
@@ -1,6 +1,13 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +28,25 @@ public class Kata5SchedulingObservable {
         // 5. messe die Laufzeit
         // 6. Füge jetzt an passender Stelle in der Observable-Chain einen Schduler ein um die Ausführungszeit zu verkürzen
 
-        WIKI_SERVICE.wikiArticleBeingReadObservable(50, TimeUnit.MILLISECONDS);
+        WaitMonitor waitMonitor = new WaitMonitor();
+        Scheduler fiveThreads = ReactiveUtil.newScheduler(50, "my-scheduler");
+
+        Observable<String> articleNames = WIKI_SERVICE.wikiArticleBeingReadObservable(1, TimeUnit.MILLISECONDS).take(20);
+
+        Observable<ParsedPage> articles = articleNames
+                .flatMap(WIKI_SERVICE::fetchArticle)
+                .subscribeOn(Schedulers.io())
+                .flatMap(WIKI_SERVICE::parseMediaWikiText).cache();
+
+        Observable<Integer> ratings = articles.flatMap(WIKI_SERVICE::rate).subscribeOn(fiveThreads);
+        Observable<Integer> wordCounts = articles.flatMap(WIKI_SERVICE::countWords).subscribeOn(fiveThreads);
+
+        Observable<String> results = Observable.zip(articles, ratings, wordCounts,
+                (a, r, wc) -> String.format("Article: %s, Rating: %s, Word Count %s", StringUtils.left(a.getText(), 20), r, wc));
+
+        results.subscribe(System.out::println, e -> e.printStackTrace(), System.out::println);
+
+        waitMonitor.waitFor(20, TimeUnit.SECONDS);
     }
 
 }
