bb25069cff50d6453a203ff0ee399b159728678e
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-18 14:41:50 +0100

    solved kata2

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata2TransformingObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata2TransformingObservable.java
@@ -1,6 +1,11 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import com.senacor.tecco.codecamp.reactive.services.WikiService;
+import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
+
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
 import org.junit.Test;
+import rx.Observable;
 
 /**
  * @author Andreas Keefer
@@ -9,11 +14,11 @@ public class Kata2TransformingObservable {
 
     @Test
     public void transformingObservable() throws Exception {
-        // 1. Benutze den WikiService (fetchArticle) und hole dir einen beliebigen Wikipedia Artikel
-        // 2. Transformiere das Ergebnis mit Hilfe von WikiService#parseMediaWikiText in eine Objektstruktur
-        // 3. gib den Wikipedia Artikel Text in der Console aus (ParsedPage.getText())
-
-        // WikiService.WIKI_SERVICE.fetchArticle()
+        WIKI_SERVICE
+                .fetchArticle("Java")
+                .doOnNext(System.out::println)
+                .flatMap(WIKI_SERVICE::parseMediaWikiText)
+                .subscribe(System.out::println);
     }
 
 }
