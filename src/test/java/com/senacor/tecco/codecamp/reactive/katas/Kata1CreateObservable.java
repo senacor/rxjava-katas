2cff542f6b9c16af23d55ca58d2cc0d46bf9e556
Alasdair Collinson <blalasaadri@googlemail.com>
2015-11-18 13:35:21 +0100

    Erste Aufgabe gelöst

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata1CreateObservable.java
@@ -1,8 +1,12 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
-import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
-import net.sourceforge.jwbf.core.contentRep.Article;
 import org.junit.Test;
+
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+import com.senacor.tecco.codecamp.reactive.services.WikipediaServiceMediaWikiBot;
+
+import net.sourceforge.jwbf.core.contentRep.Article;
+import rx.Observable;
 
 /**
  * @author Andreas Keefer
@@ -13,7 +17,29 @@ public class Kata1CreateObservable {
     public void erzeugeEinObservable() throws Exception {
         final String articleName = "Observable";
         // Erzeuge aus getArticle ein Observable
+        //        Observable.just(articleName).map(this::getArticle).map(Article::getText).subscribe(System.out::println);
 
+        Observable.<Article>create(observer -> {
+            try {
+                if (!observer.isUnsubscribed()) {
+                    observer.onNext(getArticle(articleName));
+                    observer.onCompleted();
+                }
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        }).map(Article::getText).subscribe(ReactiveUtil::print, System.err::println, () -> System.out.println("done"));
+
+        //        Observable.<String>create(observer -> {
+        //            try {
+        //                if (!observer.isUnsubscribed()) {
+        //                    observer.onNext(getArticle(articleName).getText());
+        //                    observer.onCompleted();
+        //                }
+        //            } catch (Exception e) {
+        //                observer.onError(e);
+        //            }
+        //        }).subscribe(System.out::println, System.err::println, () -> System.out.println("done"));
     }
 
     public Article getArticle(String name) {
