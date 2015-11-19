5134824f2db3202eb79efe90ae0bbc274e1ff8f6
Alasdair Collinson <blalasaadri@googlemail.com>
2015-11-19 10:02:37 +0100

    Aufgabe 8 gelöst

--- src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata8Batch.java
+++ src/test/java/com/senacor/tecco/codecamp/reactive/katas/Kata8Batch.java
@@ -1,8 +1,14 @@
 package com.senacor.tecco.codecamp.reactive.katas;
 
+import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
-import static com.senacor.tecco.codecamp.reactive.services.WikiService.WIKI_SERVICE;
+import com.senacor.tecco.codecamp.reactive.ReactiveUtil;
+import com.senacor.tecco.codecamp.reactive.WaitMonitor;
+import com.senacor.tecco.codecamp.reactive.services.WikiService;
 
 /**
  * @author Andreas Keefer
@@ -16,9 +22,21 @@ public class Kata8Batch {
         // 3. speichere die Artikel ab (WikiService.save(String)). Der service gibt die laufzeit zurück
         // 4. summiere die laufzeit der save calls und gib dies aus
 
-        WIKI_SERVICE.wikiArticleBeingReadObservableBurst();
-    }
+        WaitMonitor waitMonitor = new WaitMonitor();
+        WikiService wikiService = new WikiService();
+        Sum sum = new Sum();
 
+        WIKI_SERVICE.wikiArticleBeingReadObservableBurst() //
+            .take(2, TimeUnit.SECONDS) //
+            .map(wikiService::save) //
+            .subscribe(laufzeit -> {
+                sum.add(laufzeit);
+                ReactiveUtil.print("Laufzeit: %d ms", laufzeit);
+            }, Throwable::printStackTrace, waitMonitor::complete);
+
+        waitMonitor.waitFor(3, TimeUnit.SECONDS);
+        ReactiveUtil.print("Gesamtlaufzeit: %d ms", sum.sum);
+    }
 
     @Test
     public void batch() throws Exception {
@@ -26,6 +44,26 @@ public class Kata8Batch {
         //    Artikeln zu speichern. Beachte dabei, dass du hier potentiel einen Stream hast, du kannst also nicht warten
         //    bis der Stream alle artikel geliefert hat und dann alles in einem grossen batch abspeichern
 
-        WIKI_SERVICE.wikiArticleBeingReadObservableBurst();
+        WaitMonitor waitMonitor = new WaitMonitor();
+        WikiService wikiService = new WikiService();
+
+        WIKI_SERVICE.wikiArticleBeingReadObservableBurst() //
+            .take(2, TimeUnit.SECONDS) //
+            .buffer(500, TimeUnit.MILLISECONDS)
+            .map(wikiService::save) //
+            .reduce((l, r) ->  l + r)
+            .subscribe(laufzeit -> {
+                ReactiveUtil.print("Gesamtlaufzeit: %d ms", laufzeit);
+            }, Throwable::printStackTrace, waitMonitor::complete);
+
+        waitMonitor.waitFor(3, TimeUnit.SECONDS);
+    }
+
+    private class Sum {
+        private long sum = 0L;
+
+        void add(long summand) {
+            sum += summand;
+        }
     }
 }
