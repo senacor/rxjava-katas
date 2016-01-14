97f6178d64656f2734ef6a080f45223edabb12b6
Michael Omann <michael.omann@gmail.com>
2016-01-14 22:59:26 +0100

    move main method into test

--- src/main/java/com/senacor/tecco/reactive/services/MediaWikiTextParser.java
+++ src/main/java/com/senacor/tecco/reactive/services/MediaWikiTextParser.java
@@ -13,28 +13,8 @@ import rx.Observable;
  */
 public class MediaWikiTextParser {
 
-    private MediaWikiParserFactory pf = new MediaWikiParserFactory();
-    private MediaWikiParser parser = pf.createParser();
-
-    public static void main(String[] args) {
-        ParsedPage parsedPage = new MediaWikiTextParser().parser.parse(
-                "{{Dieser Artikel|behandelt das Jahr 42, weitere Bedeutungen finden sich unter [[42 (Begriffsklärung)]].}}\n" +
-                        "== Religion ==\n" +
-                        "\n" +
-                        "* Der [[Evangelist (Neues Testament)|Evangelist]] [[Markus (Evangelist)|Markus]] gründet laut kirchlicher Tradition den [[Patriarch von Alexandrien|Bischofssitz]] in [[Alexandria]]. \n" +
-                        "== Weblinks ==\n" +
-                        "\n" +
-                        "{{commonscat|42}}");
-        for (Link link : parsedPage.getLinks()) {
-            System.out.println("link=" + link.getTarget());
-        }
-        for (Section section : parsedPage.getSections()) {
-            System.out.println("Section: " + section.getTitle());
-            for (Link link : section.getLinks(Link.type.INTERNAL)) {
-                System.out.println(" sectionLink=" + link.getTarget());
-            }
-        }
-    }
+    private final MediaWikiParserFactory pf = new MediaWikiParserFactory();
+    private final MediaWikiParser parser = pf.createParser();
 
     public ParsedPage parse(String mediaWikiText) {
         final long start = System.currentTimeMillis();
