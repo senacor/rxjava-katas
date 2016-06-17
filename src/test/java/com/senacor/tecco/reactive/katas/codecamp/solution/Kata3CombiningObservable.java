6e85449da1f813ebe230f762ee90f4f352e3f840
Keefer, Andreas <Andreas.Keefer@senacor.com>
2016-06-17 15:49:51 +0200

    vertx examles

--- src/test/java/com/senacor/tecco/reactive/katas/codecamp/solution/Kata3CombiningObservable.java
+++ src/test/java/com/senacor/tecco/reactive/katas/codecamp/solution/Kata3CombiningObservable.java
@@ -41,7 +41,32 @@ public class Kata3CombiningObservable {
                 })
                 .subscribe(next -> print("next: %s", next),
                         Throwable::printStackTrace,
-                        () -> waitMonitor.complete());
+                        waitMonitor::complete);
+
+        waitMonitor.waitFor(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void combiningObservableOption2_zipWith() throws Exception {
+        // 1. fetch and parse Wikiarticle
+        // 2. use ratingService.rateObservable() and #countWordsObervable(). Combine both information as JSON
+        //    and print the JSON to the console. Example {"articleName": "Superman", "rating": 3, "wordCount": 452}
+
+        WaitMonitor waitMonitor = new WaitMonitor();
+
+        final String wikiArticle = "Bilbilis";
+        wikiService.fetchArticleObservable(wikiArticle)
+                .flatMap(wikiService::parseMediaWikiTextObservable)
+                .flatMap(parsedPage -> {
+                    Observable<Integer> rating = ratingService.rateObservable(parsedPage);
+                    Observable<Integer> wordCount = countService.countWordsObervable(parsedPage);
+                    return rating.zipWith(wordCount, (r, wc) -> String.format(
+                            "{\"articleName\": \"%s\", \"rating\": %s, \"wordCount\": %s}",
+                            wikiArticle, r, wc));
+                })
+                .subscribe(next -> print("next: %s", next),
+                        Throwable::printStackTrace,
+                        waitMonitor::complete);
 
         waitMonitor.waitFor(10, TimeUnit.SECONDS);
     }
