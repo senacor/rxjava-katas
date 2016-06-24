2a4e2890bee81920fce42fa1ad164b44ffc62d54
Keefer, Andreas <Andreas.Keefer@senacor.com>
2016-06-24 17:28:10 +0200

    kata description

--- src/test/java/com/senacor/tecco/reactive/katas/codecamp/Kata3CombiningObservable.java
+++ src/test/java/com/senacor/tecco/reactive/katas/codecamp/Kata3CombiningObservable.java
@@ -17,7 +17,7 @@ public class Kata3CombiningObservable {
     @Test
     public void combiningObservable() throws Exception {
         // 1. fetch and parse Wikiarticle
-        // 2. use ratingService.rateObservable() and #countWordsObervable(). Combine both information as JSON
+        // 2. use ratingService.rateObservable() and countService.countWordsObervable(). Combine both information as JSON
         //    and print the JSON to the console. Example {"articleName": "Superman", "rating": 3, "wordCount": 452}
 
         // wikiService.fetchArticleObservable()
