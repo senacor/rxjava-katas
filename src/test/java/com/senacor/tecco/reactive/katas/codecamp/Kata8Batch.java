dacc46f9d7bbfc6d3ba1342cb04fc0f7e387376e
Keefer, Andreas <Andreas.Keefer@senacor.com>
2017-01-14 11:13:39 +0100

    kata8 desciption

--- src/test/java/com/senacor/tecco/reactive/katas/codecamp/Kata8Batch.java
+++ src/test/java/com/senacor/tecco/reactive/katas/codecamp/Kata8Batch.java
@@ -1,5 +1,6 @@
 package com.senacor.tecco.reactive.katas.codecamp;
 
+import com.senacor.tecco.reactive.services.PersistService;
 import com.senacor.tecco.reactive.services.WikiService;
 import org.junit.Test;
 
@@ -9,12 +10,13 @@ import org.junit.Test;
 public class Kata8Batch {
 
     private final WikiService wikiService = new WikiService();
+    private final PersistService persistService = new PersistService();
 
     @Test
     public void withoutBatch() throws Exception {
         // 1. use WikiService#wikiArticleBeingReadObservableBurst that returns a stream of wiki article being read
         // 2. watch the stream 2 sec
-        // 3. save the article (WikiService.save(String)). The service returns the execution time
+        // 3. save the article (PersistService.save(String)). The service returns the execution time
         // 4. sum the execution time of the service calls and print the result
 
         wikiService.wikiArticleBeingReadObservableBurst();
