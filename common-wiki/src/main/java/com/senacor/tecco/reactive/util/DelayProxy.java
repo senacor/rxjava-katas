8c9617437af153ed7698a74e7e4ceb54ccc734b4
Keefer, Andreas <Andreas.Keefer@senacor.com>
2017-03-08 11:33:57 +0100

    Proxies can handle Flux and Mono

--- common-wiki/src/main/java/com/senacor/tecco/reactive/util/DelayProxy.java
+++ common-wiki/src/main/java/com/senacor/tecco/reactive/util/DelayProxy.java
@@ -2,7 +2,11 @@ package com.senacor.tecco.reactive.util;
 
 
 import org.apache.commons.lang3.ClassUtils;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.io.UncheckedIOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -27,6 +31,26 @@ public class DelayProxy extends DefaultProxyBehavior {
     private DelayProxy(Object obj, DelayFunction delayFunction) {
         super(obj);
         this.delayFunction = delayFunction;
+    }
+
+    @Override
+    protected Object handlePublisherReturnType(Object proxy, Method m, Object[] args) throws Throwable {
+        try {
+            Publisher<?> publisher = (Publisher) m.invoke(this.getTarget(), args);
+            Flux<?> res = Mono.defer(() -> Mono.just(1).delayElementMillis(delayFunction.delay(m.getName())))
+                    .flatMap(next -> publisher);
+
+            if (publisher instanceof Mono) {
+                return res.single();
+            } else if (publisher instanceof Flux) {
+                return res;
+            }
+            throw new IllegalArgumentException("Publisher not supported: " + publisher.getClass().getName());
+        } catch (InvocationTargetException e) {
+            throw e.getTargetException();
+        } catch (Exception e) {
+            throw new RuntimeException("unexpected invocation exception: " + e.getMessage());
+        }
     }
 
     @Override
