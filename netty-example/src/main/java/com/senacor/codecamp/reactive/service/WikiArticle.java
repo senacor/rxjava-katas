21af270a6ff9f5c29ceb2fd3a3bb195fd428cbc6
Keefer, Andreas <andreas.keefer@senacor.com>
2017-07-06 20:01:34 +0200

    tests

--- netty-example/src/main/java/com/senacor/codecamp/reactive/service/WikiArticle.java
+++ netty-example/src/main/java/com/senacor/codecamp/reactive/service/WikiArticle.java
@@ -1,5 +1,9 @@
 package com.senacor.codecamp.reactive.service;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Objects;
+
 /**
  * @author Andri Bremm
  */
@@ -33,4 +37,34 @@ public final class WikiArticle {
         return wordCount;
     }
 
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, content, rating, wordCount);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final WikiArticle other = (WikiArticle) obj;
+        return Objects.equals(this.name, other.name)
+                && Objects.equals(this.content, other.content)
+                && Objects.equals(this.rating, other.rating)
+                && Objects.equals(this.wordCount, other.wordCount);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("name", name)
+                .append("content", content)
+                .append("rating", rating)
+                .append("wordCount", wordCount)
+                .toString();
+    }
 }
---
+++ netty-example/src/test/java/com/senacor/codecamp/reactive/service/ArticleBeingReadServiceTest.java
@@ -0,0 +1,38 @@
+package com.senacor.codecamp.reactive.service;
+
+import com.senacor.codecamp.reactive.services.CountService;
+import com.senacor.codecamp.reactive.services.RatingService;
+import com.senacor.codecamp.reactive.services.WikiService;
+import com.senacor.codecamp.reactive.util.DelayFunction;
+import com.senacor.codecamp.reactive.util.FlakinessFunction;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Andreas Keefer
+ */
+public class ArticleBeingReadServiceTest {
+
+    private static final WikiArticle ARTICLE = new WikiArticle("Eigenwert", "REDIRECT Eigenwertproblem", 5, 2);
+
+    private ArticleBeingReadService service;
+
+    @Before
+    public void setUp() throws Exception {
+        service = new ArticleBeingReadService(WikiService.create(DelayFunction.withNoDelay(),
+                FlakinessFunction.noFlakiness(), true, "de"),
+                RatingService.create(), CountService.create());
+    }
+
+    @Test
+    @Ignore
+    public void createArticle() throws Exception {
+        service.createArticle(ARTICLE.getName())
+                .test()
+                .assertNoErrors()
+                .assertValue(ARTICLE)
+                .assertComplete();
+    }
+
+}
