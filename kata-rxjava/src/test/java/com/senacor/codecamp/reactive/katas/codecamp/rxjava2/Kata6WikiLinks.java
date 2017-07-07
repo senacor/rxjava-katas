cfc240945d59447ba1bc8aea1c92fbcaf88f9d48
Johannes Loher <johannes.loher@fg4f.de>
2017-07-07 16:33:50 +0200

    kata 6

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata6WikiLinks.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata6WikiLinks.java
@@ -2,7 +2,14 @@ package com.senacor.codecamp.reactive.katas.codecamp.rxjava2;
 
 import com.senacor.codecamp.reactive.services.WikiService;
 import com.senacor.codecamp.reactive.katas.KataClassification;
+import com.senacor.codecamp.reactive.util.WaitMonitor;
+import de.tudarmstadt.ukp.wikipedia.parser.Link;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.senacor.codecamp.reactive.katas.KataClassification.Classification.nightmare;
 
@@ -16,13 +23,32 @@ public class Kata6WikiLinks {
     @Test
     @KataClassification(nightmare)
     public void linksObservable() throws Exception {
+
+        WaitMonitor waitMonitor = new WaitMonitor();
         // 1. load and parse an arbitrary wiki article
         // 2. map all internal links of an article (parsedPage.getSections().getLinks(Link.type.INTERNAL))
         //    to an observable and print it to console <Start_Article> -> <Link/Article_Name>)doOnEach
         // 3. if that works: add a recursion (only 1 step) that loads the articles for the links and print all links contained in the article
         // 4. measure the performance and optimize the performance with scheduler
         // 5. do not print a combination <Start_Artikel> -> <Link/Artikel_Name> twice
-        //wikiService.fetchArticleObservable(...);
+        wikiService.fetchArticleObservable("Schnitzel")
+                .subscribeOn(Schedulers.io())
+                .map(wikiService::parseMediaWikiText)
+                .flatMap(page -> Observable.fromIterable(page.getSections()))
+                .flatMap(section -> Observable.fromIterable(section.getLinks(Link.type.INTERNAL)))
+                .flatMap((link) -> wikiService.fetchArticleObservable(link.getTarget())
+                        .map(wikiService::parseMediaWikiText)
+                        .flatMap(page -> Observable.fromIterable(page.getSections()))
+                        .flatMap(section -> Observable.fromIterable(section.getLinks(Link.type.INTERNAL)))
+                        .map(newLink -> String.format("%s -> %s", link.getText(), newLink.getText()))
+                        .subscribeOn(Schedulers.io())
+                )
+                .distinct()
+                //.doOnEach(System.out::println)
+                .subscribe(System.out::println,
+                        Throwable::printStackTrace,
+                        waitMonitor::complete);
+        waitMonitor.waitFor(100, TimeUnit.SECONDS);
     }
 
 }
