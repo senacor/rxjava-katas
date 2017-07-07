f8baa1f1bd24913680cc9f8721118788b6d567e5
Raphael von der Grün <raphinesse@gmail.com>
2017-07-07 13:41:25 +0200

    Kata 2

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata2BasicOperators.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata2BasicOperators.java
@@ -2,9 +2,12 @@ package com.senacor.codecamp.reactive.katas.codecamp.rxjava2;
 
 import com.senacor.codecamp.reactive.services.WikiService;
 import com.senacor.codecamp.reactive.katas.KataClassification;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
+import io.reactivex.Observable;
 import org.junit.Test;
 
 import static com.senacor.codecamp.reactive.katas.KataClassification.Classification.*;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * @author Andreas Keefer
@@ -20,7 +23,16 @@ public class Kata2BasicOperators {
         // 2. transform the result with the WikiService#parseMediaWikiText to an object structure
         //    and print out the first paragraph
 
-        // wikiService.fetchArticleObservable()
+        final String topic = "Vaporwave";
+        wikiService.fetchArticleObservable(topic)
+                .map(wikiService::parseMediaWikiText)
+                .map(page -> page.getFirstParagraph())
+                .map(p -> p.getText())
+                .test()
+                .awaitDone(1, SECONDS)
+                .assertValueCount(1)
+                .assertValue(text -> text.contains(topic))
+                .assertComplete();
     }
 
     @Test
@@ -29,7 +41,21 @@ public class Kata2BasicOperators {
         // 3. split the Article (ParsedPage.getText()) in words (separator=" ")
         // 4. sum the number of letters of all words beginning with character 'a' to the console
 
-        // wikiService.fetchArticleObservable()
+        final String topic = "Vaporwave";
+        wikiService.fetchArticleObservable(topic)
+                .map(wikiService::parseMediaWikiText)
+                .map(ParsedPage::getText)
+                .flatMap(t -> Observable.fromArray(t.split(" ")))
+                .filter(w -> w.startsWith("a"))
+                .doOnNext(System.out::println)
+                .map(String::length)
+                .doOnNext(System.out::println)
+                .reduce(0, (acc, len) -> acc + len)
+                .doOnSuccess(System.out::println)
+                .test()
+                .awaitDone(1, SECONDS)
+                .assertValueCount(1)
+                .assertComplete();
     }
 
     @Test
