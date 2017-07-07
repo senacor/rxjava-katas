509886ecdd66ed4a360553d4ee582fed01b97d2f
Oliver Rümpelein <oli_r@fg4f.de>
2017-07-07 13:08:54 +0200

    Finished RxJava Kata 2,

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata2BasicOperators.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata2BasicOperators.java
@@ -1,7 +1,10 @@
 package com.senacor.codecamp.reactive.katas.codecamp.rxjava2;
 
-import com.senacor.codecamp.reactive.services.WikiService;
 import com.senacor.codecamp.reactive.katas.KataClassification;
+import com.senacor.codecamp.reactive.services.WikiService;
+import de.tudarmstadt.ukp.wikipedia.parser.ContentElement;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
+import io.reactivex.Observable;
 import org.junit.Test;
 
 import static com.senacor.codecamp.reactive.katas.KataClassification.Classification.*;
@@ -16,16 +19,29 @@ public class Kata2BasicOperators {
     @Test
     @KataClassification(mandatory)
     public void basicsA() throws Exception {
-        // 1. Use the WikiService (fetchArticleObservable) and fetch an arbitrary wikipedia article
-        // 2. transform the result with the WikiService#parseMediaWikiText to an object structure
-        //    and print out the first paragraph
-
-        // wikiService.fetchArticleObservable()
+        Observable.fromCallable(() -> wikiService.fetchArticle("Observable"))
+                .map(wikiService::parseMediaWikiText)
+                .map(ParsedPage::getFirstParagraph)
+                .map(ContentElement::getText)
+                .subscribe(
+                        System.out::println,
+                        (error) -> System.err.println(error.getLocalizedMessage()),
+                        () -> System.out.println("Completed")
+                );
     }
 
     @Test
     @KataClassification(advanced)
     public void basicsB() throws Exception {
+        wikiService.fetchArticleObservable("Observable")
+                .map(wikiService::parseMediaWikiText)
+                .map(ParsedPage::getText)
+                .flatMap(s -> Observable.fromArray(s.split(" ")))
+                .map(String::toLowerCase)
+                .filter(s -> s.startsWith("a"))
+                .map(String::length)
+                .reduce(0, (i,j) -> i+j)
+                .subscribe(System.out::println);
         // 3. split the Article (ParsedPage.getText()) in words (separator=" ")
         // 4. sum the number of letters of all words beginning with character 'a' to the console
 
@@ -35,6 +51,17 @@ public class Kata2BasicOperators {
     @Test
     @KataClassification(hardcore)
     public void basicsC() throws Exception {
+        wikiService.fetchArticleObservable("Observable")
+                .map(wikiService::parseMediaWikiText)
+                .map(ParsedPage::getText)
+                .flatMap(s -> Observable.fromArray(s.split(" ")))
+                .map(String::toLowerCase)
+                .filter(s -> s.startsWith("a"))
+                .distinct()
+                .sorted((f,s) -> s.length() - f.length())
+                .take(10)
+                .subscribe(System.out::println);
+
         // 5. filter out redundant words beginning with 'a'
         // 6. order them by length and take only the top 10 words in length
 
