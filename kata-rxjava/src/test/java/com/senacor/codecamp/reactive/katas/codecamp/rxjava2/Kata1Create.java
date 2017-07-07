bfac4d5f2cd66ee5ca3cd038aab2f9385ea18720
Oliver Rümpelein <oli_r@fg4f.de>
2017-07-07 12:15:37 +0200

    Finished Kata 1,
    removed unneded maven plugin.

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata1Create.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata1Create.java
@@ -2,6 +2,7 @@ package com.senacor.codecamp.reactive.katas.codecamp.rxjava2;
 
 import com.senacor.codecamp.reactive.katas.KataClassification;
 import com.senacor.codecamp.reactive.services.integration.WikipediaServiceMediaWikiBot;
+import io.reactivex.Observable;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import org.junit.Test;
 
@@ -17,8 +18,14 @@ public class Kata1Create {
     public void createAnObservable() throws Exception {
         final String articleName = "Observable";
         // Create an observable from getArticle
-
-
+//        Observable.just(articleName)
+//                .map(a -> getArticle(a))
+//        Observable.fromCallable(() -> getArticle(articleName))
+        Observable.defer(() -> Observable.just(getArticle(articleName)))
+                .test()
+                .assertValueCount(1)
+                .assertValue(article -> article.getTitle().equals("Observable"))
+                .assertComplete();
     }
 
     public Article getArticle(String name) {
