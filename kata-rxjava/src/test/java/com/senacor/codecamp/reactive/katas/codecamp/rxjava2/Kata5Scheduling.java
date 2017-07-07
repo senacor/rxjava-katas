209fb642bb3ab0b14c5b36d4d2fa79dcf190c5b8
Raphael von der Grün <raphinesse@gmail.com>
2017-07-07 15:22:44 +0200

    Sequential Kata 5

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata5Scheduling.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata5Scheduling.java
@@ -4,6 +4,7 @@ import com.senacor.codecamp.reactive.katas.KataClassification;
 import com.senacor.codecamp.reactive.services.CountService;
 import com.senacor.codecamp.reactive.services.RatingService;
 import com.senacor.codecamp.reactive.services.WikiService;
+import io.reactivex.Observable;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -28,7 +29,25 @@ public class Kata5Scheduling {
         // 5. measure the runtime
         // 6. add a scheduler to a specific position in the observable chain to reduce the execution time
 
-        wikiService.wikiArticleBeingReadObservable(50, TimeUnit.MILLISECONDS);
+        long start = System.nanoTime();
+
+        wikiService.wikiArticleBeingReadObservable(50, TimeUnit.MILLISECONDS)
+                .take(20)
+                .map(wikiService::fetchArticle)
+                .map(wikiService::parseMediaWikiText)
+                .flatMap(page -> {
+                    Observable<Integer> ratings = ratingService.rateObservable(page);
+                    Observable<Integer> wordCounts = countService.countWordsObservable(page);
+                    return Observable.zip(ratings, wordCounts, (r, wc) -> String.format("{\"rating\": %s, " +
+                            "\"wordCount\": %s}", r, wc));
+                })
+                .doOnNext(System.out::println)
+                .test()
+                .awaitDone(10, TimeUnit.SECONDS);
+
+        long end = System.nanoTime();
+
+        System.out.printf("Took %s s\n", (end - start) * 1e-9);
     }
 
 }
