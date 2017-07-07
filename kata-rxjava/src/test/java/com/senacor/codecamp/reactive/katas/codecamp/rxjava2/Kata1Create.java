8f6c50b76f4d2fad7a3863c9659f17031608e9c0
Raphael von der Grün <raphinesse@gmail.com>
2017-07-07 12:18:05 +0200

    First create variant

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata1Create.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata1Create.java
@@ -2,10 +2,12 @@ package com.senacor.codecamp.reactive.katas.codecamp.rxjava2;
 
 import com.senacor.codecamp.reactive.katas.KataClassification;
 import com.senacor.codecamp.reactive.services.integration.WikipediaServiceMediaWikiBot;
+import io.reactivex.Observable;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import org.junit.Test;
 
 import static com.senacor.codecamp.reactive.katas.KataClassification.Classification.mandatory;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * @author Andreas Keefer
@@ -16,9 +18,11 @@ public class Kata1Create {
     @KataClassification(mandatory)
     public void createAnObservable() throws Exception {
         final String articleName = "Observable";
-        // Create an observable from getArticle
-
-
+        Observable.defer(() -> Observable.just(getArticle(articleName)))
+                .test()
+                .awaitDone(1, SECONDS)
+                .assertValueCount(1)
+                .assertComplete();
     }
 
     public Article getArticle(String name) {
