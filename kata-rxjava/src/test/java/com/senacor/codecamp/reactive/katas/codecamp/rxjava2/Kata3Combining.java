db6d75cf28e39243c901fb03b9617b92830f6090
Raphael von der Grün <raphinesse@gmail.com>
2017-07-07 14:04:45 +0200

    Kata 3

--- kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata3Combining.java
+++ kata-rxjava/src/test/java/com/senacor/codecamp/reactive/katas/codecamp/rxjava2/Kata3Combining.java
@@ -4,9 +4,12 @@ import com.senacor.codecamp.reactive.services.WikiService;
 import com.senacor.codecamp.reactive.katas.KataClassification;
 import com.senacor.codecamp.reactive.services.CountService;
 import com.senacor.codecamp.reactive.services.RatingService;
+import de.tudarmstadt.ukp.wikipedia.parser.ParsedPage;
+import io.reactivex.Observable;
 import org.junit.Test;
 
 import static com.senacor.codecamp.reactive.katas.KataClassification.Classification.mandatory;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * @author Andreas Keefer
@@ -24,7 +27,20 @@ public class Kata3Combining {
         // 2. use ratingService.rateObservable() and countService.countWordsObervable(). Combine both information as JSON
         //    and print the JSON to the console. Example {"articleName": "Superman", "rating": 3, "wordCount": 452}
 
-        // wikiService.fetchArticleObservable()
+        final String articleName = "Vaporwave";
+
+        Observable<ParsedPage> pageObservable = wikiService.fetchArticleObservable("Vaporwave")
+                .map(wikiService::parseMediaWikiText);
+        Observable<Integer> ratings = pageObservable.map(ratingService::rate);
+        Observable<Integer> wordCounts = pageObservable.map(countService::countWords);
+
+        Observable.zip(ratings, wordCounts, (r, wc) -> String.format("{\"articleName\": %s, \"rating\": %s, \"wordCount\": %s}", articleName, r, wc))
+                .doOnNext(System.out::println)
+                .test()
+                .awaitDone(1, SECONDS)
+                .assertValueCount(1)
+                .assertComplete();
+
     }
 
 }
